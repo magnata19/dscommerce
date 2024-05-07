ddb79b6f846165287454efad5acd898f96122f7a
magnata19 <dehpacifico@gmail.com>
2024-05-06 23:14:32 -0300

    testando endpoint da api

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -0,0 +1,16 @@
+package com.devsuperior.dscommerce.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("products")
+public class ProductController {
+
+    @GetMapping
+    public String sendMessage() {
+        return "<h1>API Funcionando!</h1>";
+    }
+
+}
--- src/main/java/com/devsuperior/dscommerce/entities/ClasseDeTesteParaCommit.java
+++
@@ -1,4 +0,0 @@
-package com.devsuperior.dscommerce.entities;
-
-public class ClasseDeTesteParaCommit {
-}
