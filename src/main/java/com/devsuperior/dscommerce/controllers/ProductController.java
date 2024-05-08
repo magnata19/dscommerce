83e6962bd78e9b6beedc56f29d931345022f4297
magnata19 <dehpacifico@gmail.com>
2024-05-07 21:55:51 -0300

    criar metodo para retornar um produto pelo id

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dscommerce.controllers;
 
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("products")
 public class ProductController {
 
-    @GetMapping
-    public String sendMessage() {
-        return "<h1>API Funcionando!</h1>";
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping(value = "{id}")
+    public ProductDTO findById(@PathVariable  Long id) {
+        ProductDTO dto = productService.findById(id);
+        return dto;
     }
 
 }
